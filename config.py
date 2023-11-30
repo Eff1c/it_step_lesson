a342ff1419e6385e3988afff8da7da7527d2abb1
eff1c <illia.martyniuk@busmarket.ua>
2023-11-30 13:23:37 +0200

    fix: Set DATABASE_HOST

--- config.py
+++ config.py
@@ -15,10 +15,11 @@ def read_bool_from_os_env(name, default=False):
 
 
 class Config:
+    DATABASE_HOST = os.environ.get("DATABASE_HOST")
     DATABASE_USER = os.environ.get("DATABASE_USER")
     DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
     DATABASE_NAME = os.environ.get("DATABASE_NAME")
-    DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:5432/{DATABASE_NAME}"
+    DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_NAME}"
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SECONDS_IN_HOUR = 60 * 60
